af5669cb4f3066da6b1dc7d104252b9c42def8ee
nonnodacciaio <nonnodacciaio@gmail.com>
2022-06-27 20:35:12 +0200

    Initial commit

---
+++ src/test/java/com/nonnodacciaio/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonnodacciaio.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
