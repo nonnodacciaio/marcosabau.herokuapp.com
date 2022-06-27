c5d1eb58612bd1c2798e5909fd6fe9f51f642924
nonnodacciaio <nonnodacciaio@gmail.com>
2022-06-27 15:49:45 +0200

    Beginning

---
+++ src/main/java/com/nonnodacciaio/portfolio/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.nonnodacciaio.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
