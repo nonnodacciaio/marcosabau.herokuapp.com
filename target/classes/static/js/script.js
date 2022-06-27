2a97a697a5da80a0c8684ff060a76828368f6a90
nonnodacciaio <nonnodacciaio@gmail.com>
2022-06-27 20:22:44 +0200

    Initial commit

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    let mywindow = $(window);
+    let mypos = mywindow.scrollTop();
+    let up = false;
+    let newscroll;
+    mywindow.scroll(function () {
+        newscroll = mywindow.scrollTop();
+        if (newscroll > mypos && !up) {
+            $('.navbar').stop().fadeOut();
+            up = !up;
+        } else if (newscroll < mypos && up) {
+            $('.navbar').stop().fadeIn();
+            up = !up;
+        }
+        mypos = newscroll;
+    });
+
+    $.fn.showTime = function () {
+        let date = new Date();
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let sec = date.getSeconds();
+        let time = hour + ":" + min + ":" + sec;
+        $(".clock").text(time);
+        setTimeout($.fn.showTime, 1000);
+    }
+
+    $("#portfolio").on("click", function () {
+        $(".modal").show();
+    })
+
+    $("#close").on("click", function () {
+        $(".modal").hide();
+    })
+
+    $.fn.showTime();
+
+});
