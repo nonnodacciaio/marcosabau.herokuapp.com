e72cec8ddee2ab28f7d6b53114d7c1893763e836
nonnodacciaio <nonnodacciaio@gmail.com>
2022-06-27 15:58:07 +0200

    Added procfile

--- src/main/java/com/nonnodacciaio/portfolio/controller/PortfolioController.java
+++ src/main/java/com/nonnodacciaio/portfolio/controller/PortfolioController.java
@@ -2,10 +2,9 @@ package com.nonnodacciaio.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class Controller {
+public class PortfolioController {
 
     @GetMapping("/")
     public String home() {
